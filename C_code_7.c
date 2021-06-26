a86f809d00d81f0efdee2fb6685ba6bf91717c7f
Tanuj <70127306+TanujTanuj@users.noreply.github.com>
2021-06-26 23:43:33 +0530

    Add files via upload

---
+++ C_code_7.c
@@ -0,0 +1,27 @@
+/*Programme --- To check wheather a number is odd or even */
+
+#include<stdio.h>
+
+int main()
+{
+   int number ;
+
+printf("Enter an integer");
+scanf("%d",&number);
+
+if( number%2 == 0){
+
+printf("Number is even");
+
+}
+
+else {
+
+printf("Number is odd ");
+
+}
+ return 0;
+}
+    
+    
+    
