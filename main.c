c8eae99d668f8d133bc81e8d4681ef29527d8d4c
Tanuj <70127306+TanujTanuj@users.noreply.github.com>
2021-06-27 04:10:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n ;
+    char c = 'R';
+    //L or R or U or D side
+    int x =0 , y = 0 ;
+    int steps = steps ;
+
+    scanf("%d %d",&n);
+
+    while (n){
+
+         switch(c) {
+
+            case 'R' :
+            x = x + steps ;
+            steps = steps + 10 ;
+            c = 'U';
+            break;
+
+            case 'U' :
+            y = y + steps ;
+            steps = steps + 10 ;
+            c = 'L';
+            break;
+
+            case 'L' :
+            x = x - steps ;
+            steps = steps + 10 ;
+            c = 'D';
+            break;
+
+            case 'D' :
+            y = y - steps ;
+            steps = steps + 10 ;
+            c = 'A';
+            break;
+
+            case 'A' :
+            x = x + steps ;
+            steps = steps + 10 ;
+            c = 'R';
+            break;
+
+         }
+         n--;
+    }
+    return 0;
+}
